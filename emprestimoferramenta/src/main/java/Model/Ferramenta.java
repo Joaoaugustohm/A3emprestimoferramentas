6acc0807b397a34940fcf00912e630559c561cd4
Joaoaugustohm <joaom.homemjah@gmail.com>
2024-05-28 14:30:00 -0300

    finalização cod

--- emprestimoferramenta/src/main/java/Model/Ferramenta.java
+++ emprestimoferramenta/src/main/java/Model/Ferramenta.java
@@ -23,3 +23,36 @@ public class Ferramenta {
         this.marca = marca;
         this.custoAquisicao = custoAquisicao;
     }
+        public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCustoAquisicao() {
+        return custoAquisicao;
+    }
+
+    public void setCustoAquisicao(double custoAquisicao) {
+        this.custoAquisicao = custoAquisicao;
+    }
+   
+}
